d266c110279992b5c74968f4a6bd8a26db61c398
jess voss <jess_wolf_24@hotmail.com>
2020-02-18 02:45:21 -0500

    Lesson 5

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -20,6 +20,10 @@ namespace contosoUniversity.Controllers
         {
             return View(db.Students.ToList());
         }
+        public ActionResult Display()
+        {
+            return View(db.Students.ToList());
+        }
 
         // GET: Student/Details/5
         public ActionResult Details(int? id)
